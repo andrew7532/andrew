cc44acc9a637c1fad53faf923127e2daa02de7e4
andrew7532 <andrewmouradossantos@gmail.com>
2025-03-02 12:41:36 -0300

    Add files via upload

---
+++ Gerador de Senha/Gerador de Senha/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Gerador_de_Senha;
+
+    while (true)
+{
+  Console.WriteLine ("--- Gerador de Senha---");
+  Console.WriteLine ("Digite 1 para gerar uma senha"); 
+  Console.WriteLine ("Digite 2 para sair");  
+        
+  string opcao = Console.ReadLine();
+        
+   if (opcao == "1")
+   {
+
+    Console.WriteLine("Digite o tamanho da senha que deseja gerar:");
+
+    int tamanhoDaSenha = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Incluir números: (s/n)");
+    bool aceitaNumero = Console.ReadLine().ToLower() == "s";
+
+    Console.WriteLine("Incluir letras maiúsculas: (s/n)");
+    bool aceitaMaiuscula = Console.ReadLine().ToLower() == "s";
+
+    Console.WriteLine("Incluir símbolo: (s/n)");
+    bool aceitaSimbolo = Console.ReadLine().ToLower() == "s";
+
+    string senhaGerada = GerarSenha(tamanhoDaSenha, aceitaMaiuscula, aceitaNumero, aceitaSimbolo);
+    Console.WriteLine($"Senha gerada: {senhaGerada}"); 
+    }
+    else if (opcao == "2")
+{
+    break;
+}
+    else
+{
+    Console.WriteLine("Opção inválida");
+}
+
+Console.WriteLine("Programa finalizado");
+Console.ReadLine();
+
+string GerarSenha (int tamanhodasenha, bool aceitaMaiuscula, bool aceitaNumero, bool aceitaSimbolo)
+{
+    string letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
+    string numeros = "0123456789";
+    string simbolo = "!@#$%&*()_+"
+}
